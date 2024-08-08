d823b2a533a243d39348f43ddb51d8f4cfaf9be1
AjestradaE <135253676+AjestradaE@users.noreply.github.com>
2024-08-08 15:02:42 -0600

    Has been added the basic form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    Error404PageComponent
+    Error404PageComponent,
+    SideMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[ 
-    Error404PageComponent
+    Error404PageComponent,
+    SideMenuComponent,
   ]
 })
 export class SharedModule { }
