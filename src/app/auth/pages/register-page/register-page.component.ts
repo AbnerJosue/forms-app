5b7fecc51aa07e37ccf094b8f1148b4a33d127d3
AjestradaE <135253676+AjestradaE@users.noreply.github.com>
2024-08-08 17:24:33 -0600

    New services added, that is using for validate the values

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from '../../../shared/validators/validators';
+// import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/service/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -11,18 +12,21 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup;
 
-  constructor(private fbs: FormBuilder) {
-    this.myForm = this.fbs.group({
-      name: ['', [ Validators.required, customValidators.firstNameAndLastnamePattern] ],
-      email: ['', [ Validators.required , customValidators.emailPattern ] ],
-      username: ['', [ Validators.required, customValidators.canBeStrider ] ],
-      password: ['', [ Validators.required, Validators.minLength( 6 ) ] ],
-      password2: ['', [ Validators.required ] ],
-    });
+  constructor(
+    private fbs: FormBuilder,
+    private validatorsService: ValidatorsService
+  ) {
+      this.myForm = this.fbs.group({
+        name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern)] ],
+        email: ['', [ Validators.required , Validators.pattern( this.validatorsService.emailPattern )] ],
+        username: ['', [ Validators.required, this.validatorsService.canBeStrider ] ],
+        password: ['', [ Validators.required, Validators.minLength( 6 ) ] ],
+        password2: ['', [ Validators.required ] ],
+      });
   }
 
   isValidField(field: string) { 
-    // TODO: obtener validacion de campo
+    return this.validatorsService.isValidField( this.myForm, field );
   }
 
   onSubmit(): void {
